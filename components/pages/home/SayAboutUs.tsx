d039ad380642ebea75c88ee13122c7b296217b12
ahmed <digital.ahmed.eg@gmail.com>
2025-12-01 23:30:44 +0200

    fix carousel

--- components/pages/home/SayAboutUs.tsx
+++ components/pages/home/SayAboutUs.tsx
@@ -1,20 +1,20 @@
 "use client"
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { RiDoubleQuotesL } from "react-icons/ri"
 import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel"
 import Autoplay from "embla-carousel-autoplay"
 import { sayAboutUs } from "@/constants/homePage"
 import { Separator } from "@/components/ui/separator"
 import Image from "next/image"
+import { QuoteIcon } from "lucide-react"
 
 export default function SayAboutUs() {
 	return (
 		<Card className="gradient text-neutral-100 h-auto mb-24 ">
-			<CardContent className="flex lg:flex-row flex-col items-center justify-between h-full">
+			<CardContent className="flex lg:flex-row flex-col lg:items-center lg:justify-between  gap-8 ">
 				{/* ----------------------------- title ----------------------------- */}
 				<div className="flex flex-col items-start justify-center  h-full gap-4 flex-1">
-					<RiDoubleQuotesL className="text-9xl " />
+					<QuoteIcon size={96}/>
 					<h2>
 						what our clients <br />
 						say about us
@@ -22,31 +22,30 @@ export default function SayAboutUs() {
 				</div>
 
 				{/* ------------------------------ Carousel ------------------------------ */}
-				{/* TODO FIX CAROUSEL IN MOBILE */}
-				<div className="lg:flex-2 flex-1 w-sm  h-full 0">
+				<div className="lg:flex-2 flex-1 w-full h-full">
 					<Carousel
 						plugins={[
 							Autoplay({
 								delay: 9000,
 							}),
 						]}
-						className=" w-fit"
+						className=" w-full"
 					>
 						<CarouselContent>
 							{sayAboutUs.map(({ image, job, name, whatSay }) => (
 								<CarouselItem key={image} className="h-full w-fit">
-									<Card className=" h-full lg:w-full w-sm">
-										<CardHeader className="space-y-2">
-											<div className="relative size-44">
+									<Card className=" h-full w-full ">
+										<CardHeader>
+											<div className="relative size-44 mb-2">
 												<Image src={image} alt={name} fill className="object-fill border rounded-2xl" />
 											</div>
 											<CardTitle>{name}</CardTitle>
-											<CardDescription>{job}</CardDescription>
-											<div className="w-6">
+											<CardDescription className="line-clamp-1">{job}</CardDescription>
+											<div className="w-48 lg:w-96">
 												<Separator className="bg-primary" />
 											</div>
 										</CardHeader>
-										<CardContent>{whatSay}</CardContent>
+										<CardContent><h6 className="line-clamp-4">{whatSay}</h6></CardContent>
 									</Card>
 								</CarouselItem>
 							))}
